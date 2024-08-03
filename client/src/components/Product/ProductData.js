245bd1d657915cdfae2d8f7d8330e928f317c45e
Tohma <praxyftw@gmail.com>
2024-08-03 18:38:38 +0200

    change image to local to prevent delete product

---
+++ client/src/components/Product/ProductData.js
@@ -0,0 +1,42 @@
+import img1 from "../../assets/images/Product/1.jpg";
+import img2 from "../../assets/images/Product/2.jpg";
+import img3 from "../../assets/images/Product/3.jpg";
+import img4 from "../../assets/images/Product/4.jpg";
+import img5 from "../../assets/images/Product/5.jpg";
+import img6 from "../../assets/images/Product/6.jpg";
+import img7 from "../../assets/images/Product/7.jpg";
+import img8 from "../../assets/images/Product/8.jpg";
+import img9 from "../../assets/images/Product/9.jpg";
+import img10 from "../../assets/images/Product/10.jpg";
+import img11 from "../../assets/images/Product/11.jpg";
+import img12 from "../../assets/images/Product/12.jpg";
+import img13 from "../../assets/images/Product/13.jpg";
+import img14 from "../../assets/images/Product/14.jpg";
+import img15 from "../../assets/images/Product/15.jpg";
+import img16 from "../../assets/images/Product/16.jpg";
+import img17 from "../../assets/images/Product/17.jpg";
+
+
+
+
+const ProductData = [   
+    {"id": 1,"title":"6 Chaises design TOP GIO - Lot de 6 - intérieur et extérieur","category":"Cuisine et Maison","image":img1,"price":392.00,"ratingStar":0,"ratingNumber":"0","prime":false,"url":"6-Chaises-design-TOP-GIO-Lot-de-6-intérieur-et-extérieur"},
+    {"id": 2,"title":"BU-1000 Chairs. Updated version","category":"Livres","image":img2,"price":14.99,"ratingStar":5,"ratingNumber":581,"prime":false,"url":"BU-1000 Chairs"},
+    {"id": 3,"title":"FabricBike Aero - Vélo Fixie, Fixed Gear, Single Speed, Cadre Aluminium et Fourche Carbone, Roues 28, 3 Tailles, 5 Couleurs, 7,95 kg (Taille M)","category":"Sports et Loisirs","image":img3,"price":699.99,"ratingStar":4,"ratingNumber":36,"prime":false,"url":"FabricBike-Aero"},
+    {"id": 4,"title":"Apple AirPods Max - Bleu Ciel","category":"High-Tech","image":img4,"price":513.18,"ratingStar":5,"ratingNumber":1201,"prime":true,"url":"Apple-AirPods-Max-Bleu-Ciel"},
+    {"id": 5,"title":"SAMSUNG UE65TU7095 TV LED UHD 4K 65 Pouces Smart TV","category":"High-Tech","image":img5,"price":499.00,"ratingStar":4,"ratingNumber":2129,"prime":true,"url":"SAMSUNG-UE65TU7095-TV-LED-UHD-4K-65-Pouces-Smart-TV"},
+    {"id": 6,"title":"Apple iPhone 12 (64 Go) - Noir","category":"High-Tech","image":img6,"price":879.00,"ratingStar":5,"ratingNumber":1111,"prime":true,"url":"Apple-iPhone-12-64Go-Noir"},
+    {"id": 7,"title":"Artemide Nessino Lampe Orange","category":"Cuisine et Maison","image":img7,"price":164.98,"ratingStar":5,"ratingNumber":241,"prime":true,"url":"Artemide-Nessino-Lampe-Orange"},
+    {"id": 8,"title":"vidaXL Table Basse Table de Salon 98x45x30 cm Verre Trempé Transparent Maison","category":"Cuisine et Maison","image":img8,"price":164.98,"ratingStar":5,"ratingNumber":241,"prime":false,"url":"vidaXL-Table-Basse-Table-de-Salon-98x45x30-cm-Verre-Trempé-Transparent-Maison"},
+    {"id": 9,"title":"Samsung Galaxy S20 - Smartphone Portable débloqué 5G (Ecran: 6,2 pouces - 128 Go - Double Nano-SIM - Android) - Gris","category":"High-Tech","image":img9,"price":750.00,"ratingStar":5,"ratingNumber":117,"prime":false,"url":"Samsung-Galaxy-S20-Smartphone-Portable-débloqué-5G-Gris"},
+    {"id": 10,"title":"250ml Ultrasonique Diffuseurs d'Huiles Essentielles Electrique ferreuse, Silencieux Humidificateur, Minuterie et Arrêt Automatique sans eau,pour la maison Spa Yoga Massage","category":"Hygiène et Santé","image":img10,"price":31.99,"ratingStar":5,"ratingNumber":639,"prime":false,"url":"250ml-Ultrasonique-Diffuseurs-d-Huiles-Essentielles-Electrique-ferreuse-Silencieux-Humidificateur"},
+    {"id": 11,"title":"Mario Kart 8 Deluxe","category":"Jeux video","image":img11,"price":69.99,"ratingStar":5,"ratingNumber":12683,"prime":false,"url":"Mario-Kart-8-Deluxe"},
+    {"id": 12,"title":"Windows 10 professionnel 32/64 bits key | Original Clé de Licence Français | 100% de garantie d'activation | [Téléchargement] | Livraison 2-6h par E-mail","category":"Logiciels","image":img12,"price":14.90,"ratingStar":5,"ratingNumber":118,"prime":false,"url":"Windows-10-professionnel-32/64-bits-key"},
+    {"id": 13,"title":"UNO jeu de société et de cartes, W2087","category":"Jeux et Jouets","image":img13,"price":7.40,"ratingStar":5,"ratingNumber":35653,"prime":true,"url":"UNO-jeu-de-société-et-de-cartes"},
+    {"id": 14,"title":"SERESTO Chien – Collier Anti-Puces et Anti-Tiques pour Grands Chiens - 8KG Et Plus","category":"Animalerie","image":img14,"price":28.99,"ratingStar":5,"ratingNumber":15294,"prime":true,"url":"SERESTO-Chien–Collier-Anti-Puces-et-Anti-Tiques-pour-Grands-Chiens-8KG-Et-Plus"},
+    {"id": 15,"title":"Fire TV Stick Lite avec télécommande vocale Alexa | Lite (sans boutons de contrôle de la TV), Streaming HD, Modèle 2020","category":"Appareils Amazon","image":img15,"price":29.99,"ratingStar":5,"ratingNumber":9519,"prime":false,"url":"Fire-TV-Stick-Lite-avec-télécommande-vocale-Alexa"},
+    {"id": 16,"title":"Eastpak Padded Pak'r Sac à Dos, 40 cm, 24 L, Noir (Black)","category":"Bagages","image":img16,"price":50,"ratingStar":5,"ratingNumber":30006,"prime":true,"url":"Eastpak-Padded-Pak'r-Sac-à-Dos-40cm-24L-Noir-"},
+    {"id": 17,"title":"Minecraft","category":"Applis & Jeux","image":img17,"price":6.99,"ratingStar":5,"ratingNumber":101569,"prime":false,"url":"Minecraft"}
+]
+
+export default ProductData;
